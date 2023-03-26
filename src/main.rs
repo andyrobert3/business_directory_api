c34956f5406409c9bd8baaa5f863317ae7564ff0
Matthew <alexandermatthew12@gmail.com>
2023-03-26 16:07:30 +0800

    feat: create business endpoint

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,23 @@ fn get_businesses() -> Json<Vec<Business>> {
     Json(businesses)
 }
 
+#[post("/business", format = "json", data = "<business>")]
+fn create_business(business: Json<Business>) -> Status {
+    let mut businesses = load_businesses();
+
+    if let Some(_index) = businesses
+        .iter()
+        .position(|item| item.business_name == business.0.business_name)
+    {
+        return Status::Conflict;
+    }
+
+    businesses.push(business.0);
+    save_businesses(&businesses);
+    Status::Created
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![get_businesses])
+    rocket::build().mount("/", routes![get_businesses, create_business])
 }
