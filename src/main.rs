313bc798963ec9e8a103e755ce9aa72a9e05975d
Matthew <alexandermatthew12@gmail.com>
2023-03-26 16:34:07 +0800

    feat: add "delete" & "update"

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,47 @@ fn create_business(business: Json<Business>) -> Status {
     Status::Created
 }
 
+#[delete("/business", format = "json", data = "<business>")]
+fn delete_business(business: Json<Business>) -> Status {
+    let mut businesses = load_businesses();
+
+    if let Some(_index) = businesses
+        .iter()
+        .position(|item| item.business_name == business.0.business_name)
+    {
+        businesses.remove(_index);
+        save_businesses(&businesses);
+        Status::NoContent
+    } else {
+        Status::NotFound
+    }
+}
+
+#[put("/business", format = "json", data = "<business>")]
+fn update_business(business: Json<Business>) -> Status {
+    let mut businesses = load_businesses();
+
+    if let Some(index) = businesses
+        .iter()
+        .position(|item| item.business_name == business.0.business_name)
+    {
+        businesses[index] = business.0;
+        save_businesses(&businesses);
+        Status::NoContent
+    } else {
+        Status::NotFound
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![get_businesses, create_business])
+    rocket::build().mount(
+        "/",
+        routes![
+            get_businesses,
+            create_business,
+            delete_business,
+            update_business
+        ],
+    )
 }
